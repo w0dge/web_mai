26ea811c620ac23ebb97548ee7248ebc039713e5
Gleb <57525153+w0dge@users.noreply.github.com>
2021-12-23 13:03:14 +0300

    update

--- pz_1/main.py
+++ pz_1/main.py
@@ -4,23 +4,20 @@ class Rectangle:
     def __init__(self, sideA, sideB):
         self.sideA = sideA
         self.sideB = sideB
-        self.area_ = self.area(sideA, sideB)
         self.__class__.all_rectangles.append(self)
 
     def area(self):
-        return self.sideA*self.sideB
+        return self.sideA * self.sideB
 
     @staticmethod
     def area_any(side_1, side_2):
-        return side_1*side_2
+        return side_1 * side_2
 
     @classmethod
     def area_all(cls):
         areas = []
-        i = 0
-        for shape in cls:
-            areas[i] = shape.area()
-            i += 1
+        for shape in cls.all_rectangles:
+            areas.append(shape.area())
         return areas
 
 
@@ -29,3 +26,9 @@ class Square(Rectangle):
         super(Square, self).__init__(side, side)
 
 
+rect = Rectangle(3, 5)
+square = Square(3)
+
+print(rect.area())
+print(square.area())
+print(rect.area_all())
